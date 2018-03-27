1e81d45ac22fee399f6647118f0efea440e1c35c
奶爸 <1@5.nu>
2018-03-27 18:05:53 +0800

    add utm_source

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -16,7 +16,7 @@ import (
 func init() {
 	// initial global settings
 	gocd.InitSysConfig("conf/app.ini")
-	unzipAssets("resource/", "4", []string{"resource"}, RestoreAssets)
+	unzipAssets("resource/", "5", []string{"resource"}, RestoreAssets)
 }
 
 func main() {
