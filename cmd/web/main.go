df4402c11a5cab57d052ae4afd437033480204e9
奶爸 <1@5.nu>
2018-03-29 16:30:09 +0800

    update assets version

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -16,7 +16,7 @@ import (
 func init() {
 	// initial global settings
 	gocd.InitSysConfig("conf/app.ini")
-	unzipAssets("resource/", "6", []string{"resource"}, RestoreAssets)
+	unzipAssets("resource/", "7", []string{"resource"}, RestoreAssets)
 }
 
 func main() {
