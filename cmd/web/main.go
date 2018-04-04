ecfed1182d024b3b5f8a8dd9eb0b57bb917b93a6
奶爸 <1@5.nu>
2018-04-04 18:16:36 +0800

    fix js

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -16,7 +16,7 @@ import (
 func init() {
 	// initial global settings
 	gocd.InitSysConfig("conf/app.ini")
-	unzipAssets("resource/", "12", []string{"resource"}, RestoreAssets)
+	unzipAssets("resource/", "13", []string{"resource"}, RestoreAssets)
 }
 
 func main() {
